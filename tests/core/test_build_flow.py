25b8b5940589bb4e574e557c4d9cb929bd04f053
perillaroc <perillaroc@gmail.com>
2022-02-09 11:02:34 +0800

    :white_check_mark: add build simple flow test.

---
+++ tests/core/test_build_flow.py
@@ -0,0 +1,57 @@
+import pytest
+
+from takler.core import Task, NodeContainer, Flow
+from takler.visitor import pre_order_travel, SimplePrintVisitor
+
+
+def test_build_flow():
+    """
+    Flow:
+
+        |- flow1 [unknown]
+          |- container1 [unknown]
+            |- task1 [unknown]
+            |- container2 [unknown]
+              |- task2 [unknown]
+              |- task3 [unknown]
+          |- task4 [unknown]
+          |- container3 [unknown]
+            |- task5 [unknown]
+          |- task6 [unknown]
+
+    """
+    flow1 = Flow("flow1")
+    container1 = flow1.add_container("container1")
+    task1 = container1.add_task("task1")
+    container2 = container1.add_container("container2")
+    task2 = container2.add_task("task2")
+    task3 = container2.add_task("task3")
+    task4 = flow1.add_task("task4")
+    container3 = flow1.add_container("container3")
+    task5 = container3.add_task("task5")
+    task6 = flow1.add_task("task6")
+
+    print()
+    pre_order_travel(flow1, SimplePrintVisitor())
+
+
+def test_build_flow_using_with():
+    with Flow("flow1") as flow1:
+        with flow1.add_container("container1") as container1:
+            with container1.add_task("task1") as task1:
+                pass
+            with container1.add_container("container2") as container2:
+                with container2.add_task("task2") as task2:
+                    pass
+                with container2.add_task("task3") as task3:
+                    pass
+            with flow1.add_task("task4") as task4:
+                pass
+        with flow1.add_container("container3") as container3:
+            with container3.add_task("task5") as task5:
+                pass
+        with flow1.add_task("task6") as task6:
+            pass
+
+        print()
+        pre_order_travel(flow1, SimplePrintVisitor())
