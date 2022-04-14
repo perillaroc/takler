f3183a5c7235252c1c8a1eb580b749f4b839c888
perillaroc <perillaroc@gmail.com>
2022-04-14 14:27:25 +0800

    :white_check_mark: add conftest for simple flow.

---
+++ tests/core/conftest.py
@@ -0,0 +1,44 @@
+import pytest
+
+from takler.core import Task, NodeContainer, Flow
+
+
+@pytest.fixture
+def flow_objects():
+    """
+    Flow:
+
+        |- flow1 [unknown]
+          |- container1 [unknown]
+            |- task1 [unknown]
+            |- container2 [unknown]
+              |- task2 [unknown]
+              |- task3 [unknown]
+          |- task4 [unknown]
+          |- container3 [unknown]
+            |- task5 [unknown]
+          |- task6 [unknown]
+
+    """
+    flow1 = Flow("flow1")
+    container1 = flow1.add_container("container1")
+    task1 = container1.add_task("task1")
+    container2 = container1.add_container("container2")
+    task2 = container2.add_task("task2")
+    task3 = container2.add_task("task3")
+    task4 = flow1.add_task("task4")
+    container3 = flow1.add_container("container3")
+    task5 = container3.add_task("task5")
+    task6 = flow1.add_task("task6")
+    return dict(
+        flow1=flow1,
+        container1=container1,
+        task1=task1,
+        container2=container2,
+        task2=task2,
+        task3=task3,
+        task4=task4,
+        container3=container3,
+        task5=task5,
+        task6=task6
+    )
--- tests/core/test_node_access.py
+++ tests/core/test_node_access.py
@@ -1,52 +1,6 @@
 from dataclasses import dataclass
 from typing import Optional
 
-import pytest
-
-from takler.core import Task, NodeContainer, Flow
-from takler.visitor import pre_order_travel, SimplePrintVisitor
-
-
-@pytest.fixture
-def flow_objects():
-    """
-    Flow:
-
-        |- flow1 [unknown]
-          |- container1 [unknown]
-            |- task1 [unknown]
-            |- container2 [unknown]
-              |- task2 [unknown]
-              |- task3 [unknown]
-          |- task4 [unknown]
-          |- container3 [unknown]
-            |- task5 [unknown]
-          |- task6 [unknown]
-
-    """
-    flow1 = Flow("flow1")
-    container1 = flow1.add_container("container1")
-    task1 = container1.add_task("task1")
-    container2 = container1.add_container("container2")
-    task2 = container2.add_task("task2")
-    task3 = container2.add_task("task3")
-    task4 = flow1.add_task("task4")
-    container3 = flow1.add_container("container3")
-    task5 = container3.add_task("task5")
-    task6 = flow1.add_task("task6")
-    return dict(
-        flow1=flow1,
-        container1=container1,
-        task1=task1,
-        container2=container2,
-        task2=task2,
-        task3=task3,
-        task4=task4,
-        container3=container3,
-        task5=task5,
-        task6=task6
-    )
-
 
 def test_node_path(flow_objects):
     assert flow_objects["flow1"].node_path == "/flow1"
