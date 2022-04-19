2cefd87e0c9502dcb237edea3c4c8f7883cf982f
perillaroc <perillaroc@gmail.com>
2022-04-19 09:45:24 +0800

    :construction: move TaklerServicer into service file. #15 #14

--- takler/server/__init__.py
+++ takler/server/__init__.py
@@ -1,14 +1,10 @@
-from takler.server.protocol import takler_pb2
-from takler.server.protocol.takler_pb2_grpc import TaklerServerServicer
+
+from .service import TaklerService
 
 
-class TaklerServer(TaklerServerServicer):
-    async def RunInitCommand(self, request, context):
-        node_path = request.child_options.node_path
-        task_id = request.task_id
+class TaklerServer:
+    def __init__(self):
+        pass
 
-        print(f"INIT: {node_path} with {task_id}")
-        return takler_pb2.ServiceResponse(
-            flag=0,
-            message="",
-        )
+
+
