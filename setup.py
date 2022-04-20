5cda67bdfa596636a694ffc6b2bd2aed2ffe5ec2
perillaroc <perillaroc@gmail.com>
2022-04-20 14:40:30 +0800

    :heavy_plus_sign: add requires package for server and cli.

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
     ],
 
     keywords='workflow',
@@ -46,11 +47,12 @@ setup(
 
     install_requires=[
         'lark',
-        # 'thrift',
-        # 'pyparsing'
+        'grpcio',
+        'typer',
     ],
 
     extras_require={
+        'log': ['loguru'],
         'tests': ['pytest'],
     },
 
