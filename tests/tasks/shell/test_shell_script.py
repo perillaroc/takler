e2bc013571a98738f766a3e2a5c85b73146d39f1
perillaroc <perillaroc@gmail.com>
2022-05-05 10:41:45 +0800

    :white_check_mark: add test for ShellScript render. #22

---
+++ tests/tasks/shell/test_shell_script.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+
+import pytest
+
+from takler.core import Bunch, Flow, Task
+from takler.tasks import ShellScriptTask
+from takler.tasks.shell.shell_script import ShellScript
+
+
+@pytest.fixture
+def scripts_directory():
+    return Path(Path(__file__).parent, "scripts")
+
+
+@pytest.fixture
+def takler_home_directory():
+    return Path(Path(__file__).parent, "takler_home")
+
+
+@pytest.fixture
+def takler_include_directory():
+    return Path(Path(__file__).parent, "include")
+
+
+def test_simple_script_render(scripts_directory, takler_home_directory):
+    task1_script_path = str(Path(scripts_directory, "task1.takler"))
+    task1 = ShellScriptTask("task1", task1_script_path)
+    task1.add_parameter("TAKLER_HOME", str(takler_home_directory))
+    task1.add_parameter("SLEEP", 30)
+    task1.update_generated_parameters()
+
+    shell_script = ShellScript(
+        script_path=task1_script_path,
+        node=task1
+    )
+    shell_script.render()
+
+
+def test_script_with_include_render(scripts_directory, takler_home_directory, takler_include_directory):
+    with Bunch("nwpc_op") as bunch:
+        with Flow("flow1") as flow1:
+            bunch.add_flow(flow1)
+            task1_script_path = str(Path(scripts_directory, "task1_with_include.takler"))
+            with flow1.add_task(ShellScriptTask("task1", task1_script_path)) as task1:
+                task1.add_parameter("TAKLER_HOME", str(takler_home_directory))
+                task1.add_parameter("TAKLER_INCLUDE", str(takler_include_directory))
+                task1.add_parameter("SLEEP", 30)
+                task1.update_generated_parameters()
+
+    shell_script = ShellScript(
+        script_path=task1_script_path,
+        node=task1
+    )
+    shell_script.render()
