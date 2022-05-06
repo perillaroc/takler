20821c29773c6f7b9d7ef83e419bb1ca36f305be
perillaroc <perillaroc@gmail.com>
2022-05-06 11:36:03 +0800

    :alembic: try to run command in subprocess using async functions. #23

--- takler/tasks/shell/shell_runner.py
+++ takler/tasks/shell/shell_runner.py
@@ -1,9 +1,18 @@
 import os
 import sys
+import asyncio
+
+from anyio import run_process
 
 
 class ShellRunner:
     def spwan(self, command: str):
+        async def run_shell_command():
+            await run_process(["/bin/sh", "-c", command])
+        loop = asyncio.get_running_loop()
+        t = loop.create_task(run_shell_command())
+
+    def spwan_v2(self, command: str):
         """
         Double fork to run shell command `command` to avoid zombie process.
 
@@ -25,10 +34,17 @@ class ShellRunner:
         try:
             pid = os.fork()
             if pid > 0:
+
+                # loop = asyncio.get_running_loop()
+                # loop.stop()
+
                 # exit from second parent
                 sys.exit(0)
         except OSError:
             pass
+
+        # loop = asyncio.get_running_loop()
+        # loop.stop()
 
         os.execl("/bin/sh", "sh", "-c", command)
 
